63a72114a717cc09677da0cb209e8187b0f16f82
maciohslima <macio-helton@outlook.com>
2021-05-22 13:15:38 -0300

    "codigo 2"

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main (String[] args) {
         System.out.print("Meu primeiro programa em java");
+        System.out.println("Segunda linha de Java");
     }
 }
